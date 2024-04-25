db022b53257eeaac7e0abb2dea05af74a2cdc4fe
richardb135 <ritchiewb@gmail.com>
2024-04-25 12:35:24 +0100

    conversion to pocketbase

---
+++ src/components/back-end/databasing.jsx
@@ -0,0 +1,7 @@
+export function setTransaction(){
+
+}
+
+export function addItem(){
+    
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { configureFirebase } from "./firebase-config";
 
-configureFirebase()
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
